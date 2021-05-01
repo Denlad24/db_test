5c5b9f7a1750b127c3c3b83d0281de9441dd8544
Denlad24 <55156774+Denlad24@users.noreply.github.com>
2021-05-01 14:48:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-export DATABASE_URL = 'postgres://ltyvarlpoojqtx:520cec160d1260d86a7956270c927a440ffebea0cec7d3f4b87f1691dd794a68@ec2-54-167-152-185.compute-1.amazonaws.com:5432/d627btjo725lac'
+export DATABASE_URL = 'postgres://sypsprnkeadebh:68ccbb16afc1b1ba7c9646f06a6e16e581de34594a3e9c9cf91fdda0d2a9fdbe@ec2-54-163-254-204.compute-1.amazonaws.com:5432/dfp0du7i5gcvjd'
